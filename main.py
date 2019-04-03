b316abb58bf0b5f49de8ce4680cb08dd5d30afbe
Phillip Vo <phillip.t.vo@gmail.com>
2019-04-03 13:01:56 -0400

    Modified main script to overplot various main sequences

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-from starClass_final_moreModed import bisection
+from starClass import bisection
 import pickle
 
 massLimit = 100
@@ -24,10 +24,12 @@ R_sun = 6.9951e8
 # Range of T_c for Main Sequence
 rng_T = np.linspace(3e6,30e6,50)
 
+M_bh = 1e-6
+
 def solveStar(T):
     
     print("********** Creating star with Tc = {} **************".format(T))
-    star_optimized = bisection(rho_c_min, rho_c_max, T, massLimit, radiusLimit, N)
+    star_optimized = bisection(rho_c_min, rho_c_max, T, massLimit, radiusLimit, N, M_bh)
     if star_optimized is not None:
         pickle.dump( star_optimized, open( "stars/{}.p".format(T), "wb" ) )
     
@@ -43,28 +45,56 @@ def solveAllStars(T_lst):
     
     print("Time elapsed is {} s".format(time() - start))
 
-def main_sequence():
+def main_sequence(path="MS_stars", fig=None, axes=None):
     import os
-    star_lst = os.listdir("stars")
+    star_lst = os.listdir(path)
+    name = os.path.basename(path)
+    
     BV = np.array([])
     L = np.array([])
     stars = np.array([])
 
     for s in star_lst:
-        tmp_star = pickle.load( open("stars/{}".format(s), "rb") )
+        tmp_star = pickle.load( open("{}/{}".format(path, s), "rb") )
         BV = np.append(BV, tmp_star.T[-1])
         L = np.append(L, tmp_star.L[-1])
         stars = np.append(stars, tmp_star)
 
-    fig, axes = plt.subplots()
-    axes.scatter(np.log10(BV), np.log10(L/L_sun))
+    if axes is None:
+        fig, axes = plt.subplots()
+    
+    axes.plot(np.log10(BV), np.log10(L/L_sun), '.', label=name)
     
     axes.set_xlim(4.5,3.0)
     axes.set_xlabel(r'T (K)')
     axes.set_ylabel(r'$\frac{L}{L_\odot}$')
     axes.grid()
+    if axes is None:
+        fig.savefig('{}'.format(os.path.basename(path)))
+    return axes
+
+def main_sequence_TZ(wMS=False, overplot=False):
+    template = "stars(1e-{})"
+    seqs = [2,3,4,5,9]
+    
+    fig, axes = plt.subplots()
+    
+    if wMS:
+        main_sequence(fig=fig, axes=axes)
+    
+    for s in seqs:
+        if overplot:
+            main_sequence(template.format(s), fig, axes)
+        else:
+            main_sequence(template.format(s))
+    
+    axes.legend()
+    if overplot:
+        fig.savefig('Overplot.png')
     plt.show()
-    return stars
+
+def load_star(path, Tc):
+    return pickle.load( open("{}/{}.p".format(path, Tc), "rb"))
 
 def plotAll(star):
     plot(star)
